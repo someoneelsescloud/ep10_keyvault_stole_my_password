da6497d88517ef71b99dd07ff74085ca54bdcf6a
Sunny <sunny@someoneelsescloud.com>
2021-07-31 15:26:36 +1000

    cleaned up script

--- scripts/ExportKVpfx/exportKVpfx.ps1
+++ scripts/ExportKVpfx/exportKVpfx.ps1
@@ -9,12 +9,19 @@ param (
 )
 
 $path = (Get-Location).Path
+
+Write-Output "Retrieving the stored secret from Azure KeyVault.."
 $securePassword = Get-AzKeyVaultSecret -VaultName $vaultName -Name $certSecret -AsPlainText
+
+Write-Output "Retrieving the secret from Certficate in Azure KeyVault.."
 $secret = Get-AzKeyVaultSecret -VaultName $vaultName -Name $certificateName -AsPlainText
+
+Write-Output "Converting retrieved secret.."
 $secretByte = [Convert]::FromBase64String($secret)
+
+Write-Output "Creating PFX certifcate and setting new password with retrieved secret.."
 $x509Cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($secretByte, "", "Exportable,PersistKeySet")
 $type = [System.Security.Cryptography.X509Certificates.X509ContentType]::Pfx
 $pfxFileByte = $x509Cert.Export($type, $securePassword)
 [System.IO.File]::WriteAllBytes("$path\$certificateName.pfx", $pfxFileByte)
 
-Write-Output "$securePassword"
