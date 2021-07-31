3a2f54fc403001adff0c51de9208b4f2448f308e
Sunny <sunny@someoneelsescloud.com>
2021-07-31 15:30:59 +1000

    changed cert export name

--- scripts/ExportKVpfx/exportKVpfx.ps1
+++ scripts/ExportKVpfx/exportKVpfx.ps1
@@ -8,6 +8,7 @@ param (
     [string]$certSecret
 )
 
+# Set current folder path for new file export
 $path = (Get-Location).Path
 
 Write-Output "Retrieving the stored secret from Azure KeyVault.."
@@ -23,5 +24,5 @@ Write-Output "Creating PFX certifcate and setting new password with retrieved se
 $x509Cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($secretByte, "", "Exportable,PersistKeySet")
 $type = [System.Security.Cryptography.X509Certificates.X509ContentType]::Pfx
 $pfxFileByte = $x509Cert.Export($type, $securePassword)
-[System.IO.File]::WriteAllBytes("$path\$certificateName.pfx", $pfxFileByte)
+[System.IO.File]::WriteAllBytes("$path\export-$certificateName.pfx", $pfxFileByte)
 
